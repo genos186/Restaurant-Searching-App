129c01d6e2183cd52c3ce2d154106318346ba4ed
Amogh <48126450+genos186@users.noreply.github.com>
2020-10-22 23:37:05 +0530

    Add files via upload

--- food/Src/Screens/SearchScreen.js
+++ food/Src/Screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View , Text , StyleSheet , ScrollView } from 'react-native';
 import SearchBar from '../Components/SearchBar';
 import useResults from '../Hooks/useResults';
 import ResultsList from '../Components/ResultsList';
 
 const SearchScreen = () => {
+
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
@@ -16,7 +17,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View>
+    < >
 
       <SearchBar
         term={term}
@@ -24,12 +25,24 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
       
-    </View>
+      <ScrollView>
+
+        <ResultsList 
+          results={filterResultsByPrice('$')} 
+          title="Cost Effective"
+           />
+        <ResultsList 
+          results={filterResultsByPrice('$$')} 
+          title="Bit Pricier"
+           />
+        <ResultsList 
+          results={filterResultsByPrice('$$$')} 
+          title="Big Spender"
+           />
+      </ScrollView>
+      
+    </>
   );
 };
 
