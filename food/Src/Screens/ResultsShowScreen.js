8054b7a0dcac2f5da65ae96f1e19e8701202d9a2
Amogh <48126450+genos186@users.noreply.github.com>
2020-10-22 23:32:25 +0530

    Add files via upload

---
+++ food/Src/Screens/ResultsShowScreen.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet , Text , View , FlatList , Image } from 'react-native';
+import yelp from '../API/Yelp';
+
+export default function ResultsSHowScreen ({ navigation }) {
+	const [result,setResult]= useState(null);
+	const id=navigation.getParam('id');
+
+	const getResult = async (id) => {
+		const response= await yelp.get(`/${id}`);
+		response.data;
+		setResult(response.data);
+	};
+	useEffect(() => {
+		getResult(id);
+	}, []);
+
+	if (!result) {
+		return null;
+	}
+
+	return (
+	     <View>
+	 		<Text> {result.name} </Text>
+	 		<FlatList 
+	 			data={result.photos}
+	 			keyExtractor={(photo) => photo}
+	 			renderItem={({ item }) => {
+	 				return <Image style={styles.image} source={{ uri:item }} />
+	 			}}
+	 		/>
+	     </View>
+	);
+};
+
+const styles = StyleSheet.create({
+	image: {
+		height:200 ,
+		width:300 ,
+	}
+});
+
