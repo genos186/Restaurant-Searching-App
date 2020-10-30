a43efd66cf71795e4d4cb037f1b45ece9a0394f9
Amogh <48126450+genos186@users.noreply.github.com>
2020-10-30 10:17:44 +0530

    Update App.js

--- food/App.js
+++ food/App.js
@@ -2,14 +2,16 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './Src/Screens/SearchScreen';
 import ResultsShowScreen from './Src/Screens/ResultsShowScreen';
+import CitySearch from './Src/Screens/CitySearch';
 
-const navigator = createStackNavigator (
+const Navigator = createStackNavigator (
   {
     Search: SearchScreen,
     ResultsShow:ResultsShowScreen,
+    City:CitySearch,
   }, 
   {
-    initialRouteName: 'Search',
+    initialRouteName: 'City',
     defaultNavigationOptions: 
     {
       title: 'Restaurant Search'
