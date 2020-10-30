39d4a55be786435c5ea235c3a698aaa2cc1a0bd0
Amogh <48126450+genos186@users.noreply.github.com>
2020-10-30 10:18:54 +0530

    Update CitySearch.js

--- food/Src/Screens/CitySearch.js
+++ food/Src/Screens/CitySearch.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { StyleSheet , Text , View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import LocationBar from '../Components/LocationBar';
+import useResults from '../Hooks/useResults';
 
-export default function CitySearch() {
+export default function CitySearch({ navigation }) {
+	const [term, setTerm] = useState('');
+  	const [searchApi, results, errorMessage] = useResults();
 	return (
 	     <View>
-	 		<Text> </Text>
+	 		
+	     	<MaterialIcons 
+	     		name="restaurant-menu" 
+	     		size={28} 
+	     		color="black"
+	     		style={alignItems= 'center', justifyContent= 'flex-start'} />
+	     	<LocationBar
+        		term={term}
+        		onTermChange={setTerm}
+        		onTermSubmit={() => searchApi(term)} />
+      	  	{errorMessage ? <Text>{errorMessage}</Text> : null}
+
 	     </View>
 	);
 };
