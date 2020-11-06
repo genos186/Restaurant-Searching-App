2621c51bb539536dcb0b60e3fc166f28cac03219
Amogh <48126450+genos186@users.noreply.github.com>
2020-11-06 15:33:43 +0530

    Update App.js

--- food/App.js
+++ food/App.js
@@ -1,14 +1,18 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import 'react-native-gesture-handler';
+
 import SearchScreen from './Src/Screens/SearchScreen';
 import ResultsShowScreen from './Src/Screens/ResultsShowScreen';
 import CitySearch from './Src/Screens/CitySearch';
+import useResults from './Src/Hooks/useResults';
 
 const Navigator = createStackNavigator (
   {
     Search: SearchScreen,
     ResultsShow:ResultsShowScreen,
     City:CitySearch,
+    Api:useResults,
   }, 
   {
     initialRouteName: 'City',
